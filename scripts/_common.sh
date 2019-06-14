b71ea4c266f8697c86d4b16d45129ce84f2e5682
yalh76 <yalh@yahoo.com>
2019-06-14 21:05:04 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -37,7 +37,7 @@ ynh_composer_exec () {
 
 	COMPOSER_HOME="$workdir/.composer" \
 		php${phpversion} "$workdir/composer.phar" $commands \
-		-d "$workdir" --quiet --no-interaction
+		-d "$workdir" --verbose --no-interaction
 }
 
 # Install and initialize Composer in the given directory
